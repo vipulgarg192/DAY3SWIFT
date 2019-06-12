8a38ee93db4d9a6f3015eb52d4f91966885e5164
VIPUL <VIPULGARG192@GMAIL.COM>
2019-06-11 21:18:06 -0400

    this code is for dictionaries

--- DAY3SWIFT/main.swift
+++ DAY3SWIFT/main.swift
@@ -102,6 +102,24 @@ let count = unSortedValue.count
 //    }
 //}
 
+
+//func printTheReverseArray(arr: [String],index : Int) {
+//
+//    if arr.isEmpty {
+//        return
+//    }else{
+//        if index == -1
+//        {
+//            return
+//        }else{
+//            print(arr[index])
+//            printTheReverseArray(arr: arr, index: index-1)
+//        }
+//
+//    }
+//}
+
+
 func printTheReverseArray(arr: [String],index : Int) {
     
     if arr.isEmpty {
@@ -174,3 +192,90 @@ print(SumN(n: 10))
 
 
 
+/////
+
+// Dictonarairy
+var dict = Dictionary<String, String>()
+
+dict["IND"] = "India"
+dict["CND"] = "CANADA"
+dict["PAK"] = "Pakistan"
+dict["USA"] = "United States Of America"
+dict["afg"] = "afgan"
+
+print(dict)
+print(dict["USA"]!)
+
+
+var countryDict = ["a":"america","b":"Boy","c":"Cat"]
+print(countryDict)
+if let c = countryDict["a"]{
+    print(c)
+}
+print(countryDict["d"] ?? "Key doesnot exits")
+
+
+for res in countryDict{
+    print("dict: ",res)
+}
+
+for (_,v) in countryDict{
+    print(v)
+}
+
+var newArray : Array<Int>
+
+
+var mySet = Set<String>()
+mySet.insert("India")
+mySet.insert("Pakistan")
+mySet.insert("Canada")
+mySet.insert("Russia")
+
+
+var mySet2 = Set<String>()
+mySet2.insert("USA")
+mySet2.insert("SriLanka")
+mySet2.insert("India")
+mySet2.insert("Nepal")
+
+let interSet = mySet.intersection(mySet2)
+print("Common: ",interSet)
+
+
+let unionSet = mySet.union(mySet2)
+print("union: ",unionSet)
+
+let disJointSet = mySet.isDisjoint(with: mySet2)
+print("disJoint: ",disJointSet)
+
+for x in mySet{
+    print(x)
+}
+
+var anyTypeDict = Dictionary<String,Any>()
+anyTypeDict["INT"] = 100
+anyTypeDict["String"] = "Hello World"
+anyTypeDict["Dict"] = ["k":"v"]
+anyTypeDict["Tuple"] = (100,200)
+
+for value in anyTypeDict{
+    if value.value is Int{
+        print("i am an intger: \(value)")
+    }
+    
+    if value.value is String{
+        print("i am an String: \(value)")
+    }
+    
+//    if value.value is ()
+    
+    if value.value is Dictionary<String, Any>{
+        print("I am an Dict: \(value)")
+    }
+    
+    if value.value is (Int,Int){
+        
+        print("I am tupe: \(value)")
+    }
+}
